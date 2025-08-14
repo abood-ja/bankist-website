e900c3f1de3999cecb63d47daa660fd08b5deb09
abood.jarrar <abd.a.jarrar@gmail.com>
2025-08-14 11:12:45 +0300

    implementing smooth scrolling on "learn more" button

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -30,4 +31,17 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+///////// handling the scroll button
+btnScroll.addEventListener('click', function (e) {
+  const s1coor = section1.getBoundingClientRect();
+  console.log(s1coor);
+
+  // window.scrollTo(s1coor.left, s1coor.top + window.scrollY);
+  // window.scrollTo({
+  //   left: s1coor.left + window.scrollX,
+  //   top: s1coor.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
